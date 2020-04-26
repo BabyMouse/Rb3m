75d3ffd30bb213bc4f26c7821a370122f9a24317
BabyMouse <babymouse011@gmail.com>
2020-04-26 23:23:51 +0700

    Add init file for Rb3m template package.

---
+++ .init_vscode_editor.ps1
@@ -0,0 +1,2 @@
+npm install prettier --save-dev --save-exact
+npm install eslint eslint-config-prettier eslint-plugin-prettier --save-dev
---
+++ .prettierrc.js
@@ -0,0 +1,51 @@
+/**
+ * ref: https://prettier.io/docs/en/configuration.html
+ * ref: https://prettier.io/docs/en/options.html
+ */
+module.exports = {
+  overrides: [
+    {
+      files: ['*.html'],
+      options: {
+        printWidth: 180,
+        tabWidth: 2,
+        //Print semicolons at the ends of statements.
+        semi: true,
+        //Print trailing commas wherever possible when multi-line.
+        //(A single - line array, for example, never gets trailing commas.)
+        trailingComma: 'none',
+        bracketSpacing: false,
+        singleQuote: true
+      }
+    },
+    {
+      files: ['*.js', '*.prettierrc.js'],
+      options: {
+        printWidth: 180,
+        tabWidth: 2,
+        semi: true,
+        trailingComma: 'none',
+        bracketSpacing: false,
+        singleQuote: true
+      }
+    },
+    {
+      files: ['*.css'],
+      options: {
+        printWidth: 180,
+        tabWidth: 2,
+        bracketSpacing: true,
+        singleQuote: true
+      }
+    },
+    {
+      files: ['*.json', '*.code-workspace', '.eslintrc'],
+      options: {
+        printWidth: 150,
+        tabWidth: 2,
+        trailingComma: 'none',
+        singleQuote: true
+      }
+    }
+  ]
+};
