fd820ef89439a4ba3105c370829b0e810dddeb60
BabyMouse <babymouse011@gmail.com>
2020-04-27 03:09:43 +0700

    Update.

--- .init_vscode_editor.ps1
+++ .init_vscode_editor.ps1
@@ -1,3 +1,5 @@
 # Init for VSCode editor
+# ref: https://github.com/PowerShell/PowerShell
+# ref: https://docs.microsoft.com/en-us/powershell/scripting/how-to-use-docs?view=powershell-7
 npm install prettier --save-dev --save-exact
 npm install eslint eslint-config-prettier eslint-plugin-prettier --save-dev
