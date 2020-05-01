b6184176f7d946b9f164cf6867e5b187b010b92e
BabyMouse <babymouse011@gmail.com>
2020-05-02 05:08:11 +0700

    Update.

--- R/toHTML.R
+++ R/toHTML.R
@@ -28,56 +28,38 @@ toHTML <- function(toc = FALSE,
                    ...) {
   df_print <- "kable"
   keep_md <- TRUE
-  template <- "templates/pandoc_template_default.html"
   theme <- NULL
-  highlight <- "templates/pandoc_highlight_haddock.theme"
   mathjax <- NULL
-  css <- c(
-    "includes/style.css",
-    "includes/style_navbar.css",
-    "includes/style_highlight.css",
-    "includes/style_code_btn.css"
-  )
-  in_header <- c(
-    "includes/header.html",
-    "includes/header_navbar.js.html",
-    "includes/header_codefolding.js.html",
-    "includes/header_sourceembed.js.html"
-  )
-  before_body <- c(
-    "includes/body_prefix.html",
-    "includes/body_prefix_navbar.html"
-  )
-  after_body <- c("includes/body_suffix.html")
-  includes <- rmarkdown::pandoc_include_args(
-    in_header = in_header,
-    before_body = before_body,
-    after_body = after_body
-  )
-
-  knitr_options <- rmarkdown::knitr_options_html(
-    fig_width = fig_width,
-    fig_height = fig_height,
-    fig_retina = fig_retina,
-    keep_md = keep_md,
-    dev = dev
-  )
-  knitr_options$opts_chunk$echo <- FALSE
-  knitr_options$opts_chunk$warning <- FALSE
-  knitr_options$opts_chunk$message <- FALSE
-  knitr_options$opts_chunk$comment <- NA
-  knitr_options$opts_chunk$R.options <- list(width = 70)
-  knitr_options$opts_knit$bookdown.internal.label <- TRUE
-  knitr_options$opts_hooks <- list()
-  knitr_options$opts_hooks$preview <- NULL
-  knitr_options$knit_hooks <- list()
-  knitr_options$knit_hooks$chunk <- NULL
   rmarkdown::output_format(
-    knitr = knitr_options,
+    knitr = rmarkdown::knitr_options_html(
+      fig_width = fig_width,
+      fig_height = fig_height,
+      fig_retina = fig_retina,
+      keep_md = keep_md,
+      dev = dev
+    ),
     pandoc = rmarkdown::pandoc_options(
       to = "html5",
-       from = rmarkdown::from_rmarkdown(fig_caption, md_extensions),
-      args = "--standalone"
+      from = rmarkdown::from_rmarkdown(
+        fig_caption,
+        md_extensions
+      ),
+      args = c(
+        "--standalone",
+        "--template", file.path(system.file("templates/toHTML", package = "Rb3m"), "pandoc_template_default.html5.html"),
+        "--highlight-style", file.path(system.file("templates/toHTML", package = "Rb3m"), "pandoc_highlight_haddock.theme"),
+        "--css", file.path(system.file("includes/toHTML", package = "Rb3m"), "style.css"),
+        "--css", file.path(system.file("includes/toHTML", package = "Rb3m"), "style_navbar.css"),
+        "--css", file.path(system.file("includes/toHTML", package = "Rb3m"), "style_highlight.css"),
+        "--css", file.path(system.file("includes/toHTML", package = "Rb3m"), "style_code_btn.css"),
+        "--include-in-header", file.path(system.file("includes/toHTML", package = "Rb3m"), "header.html"),
+        "--include-in-header", file.path(system.file("includes/toHTML", package = "Rb3m"), "header_navbar.js.html"),
+        "--include-in-header", file.path(system.file("includes/toHTML", package = "Rb3m"), "header_sourceembed.js.html"),
+        "--include-before-body", file.path(system.file("includes/toHTML", package = "Rb3m"), "body_prefix.html"),
+        "--include-before-body", file.path(system.file("includes/toHTML", package = "Rb3m"), "body_prefix_navbar.html"),
+        "--include-after-body", file.path(system.file("includes/toHTML", package = "Rb3m"), "body_suffix.html"),
+        "--variable", "code_menu"
+      )
     ),
     keep_md = keep_md,
     clean_supporting = self_contained,
@@ -89,14 +71,10 @@ toHTML <- function(toc = FALSE,
     post_processor = NULL,
     on_exit = NULL,
     base_format = rmarkdown::html_document_base(
-      smart = TRUE,
+      # smart = TRUE,
       theme = theme,
-      highlight = highlight,
       mathjax = mathjax,
-      template = template,
-      css = css,
-      includes = includes,
-      pandoc_args = c("--variable", "code_menu"),
+      pandoc_args = NULL,
       ...
     )
   )
