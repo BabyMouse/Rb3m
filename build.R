36d999028594a0813d58aff777fd043440e1b8e6
BabyMouse <babymouse011@gmail.com>
2020-05-07 02:12:26 +0700

    Update.

--- build.R
+++ build.R
@@ -5,12 +5,14 @@ devtools::load_all()
 devtools::document()
 
 # Build full options to remove unused file and folder
-#devtools::install(pkg = ".")
+# devtools::install(pkg = ".")
 devtools::install(pkg = ".", quick = TRUE)
 
 # Restart R session
 #' @seealso: https://stackoverflow.com/questions/15666810/restart-r-within-rstudio
-#.rs.api.restartSession()
+# .rs.api.restartSession()
 
 #' @seealso: https://www.rdocumentation.org/packages/startup/versions/0.14.0/topics/restart
 rstudioapi::restartSession()
+
+message(format(Sys.time(), "%Y-%m-%d"))
