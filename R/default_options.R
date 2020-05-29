441bc66766de19e226a8df1b0b287e632491cf0b
BabyMouse <babymouse011@gmail.com>
2020-05-30 03:17:42 +0700

    Add new file.

---
+++ R/default_options.R
@@ -0,0 +1,32 @@
+default_options <- list(
+  code_folding = "show",
+  is_folding = TRUE,
+  nav = list(
+    toc_deep = 3,
+    toc_icon = "list-ul-solid.svg",
+    toc_text = "",
+    toc = list(
+      current = character(0)
+    ),
+    home = list(
+      icon = "home-solid.svg",
+      text = "Home",
+      href = "https://rpubs.com/BabyMouse"
+    ),
+    contact = list(
+      icon = "github-brands.svg",
+      text = "GitHub",
+      href = "https://github.com/BabyMouse/Rb3m"
+    ),
+    show_all_code = TRUE,
+    download_code = TRUE,
+    download_file_name = character(0),
+    download_file_title = character(0)
+  ),
+  template = "default",
+  highlight = "default",
+  navbar = "default",
+  head_tempfile = character(0),
+  body_prefix = character(0),
+  body_suffix = character(0)
+)
